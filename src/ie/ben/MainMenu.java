f5c8a5ed33676d54ebf867cd8d451e745071c31c
benjimimac <c14723525@mydit.ie>
2016-03-24 14:45:38 +0000

    Start of the Main Menu

---
+++ src/ie/ben/MainMenu.java
@@ -0,0 +1,119 @@
+package ie.ben;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Insets;
+
+import javax.swing.JPanel;
+
+class DrawPanel extends JPanel {
+
+	private void drawMenuBoard(Graphics graphic) {
+
+		// Graphics2D menuGraphic = (Graphics2D) graphic;
+
+		// menuGraphic.setColor(Color.green);
+
+		// graphic.setColor(new Color(255, 0, 0));
+		// graphic.drawRect(50, 50, 100, 100);
+
+		// Draw the tiles on the board
+		for (int i = 0; i < ReversiGame.TILES_PER; i++) {
+
+		}
+
+		// menuGraphic.setColor(new Color(255, 0, 0));
+		// menuGraphic.drawRect(insets.left, insets.top, menuWidth, menuHeight);
+	}
+
+	@Override
+	public void paintComponent(Graphics g) {
+
+		super.paintComponent(g);
+		drawMenuBoard(g);
+	}
+}
+
+public class MainMenu extends JPanel {
+
+	private int menuWidth;
+	private int menuHeight;
+
+	public MainMenu() {
+
+		setPreferredSize(new Dimension(300, 300));
+
+	}
+
+	// public final void initUI() {
+	//
+	// DrawPanel main = new DrawPanel();
+	// add(main);
+	// }
+
+	@Override
+	public void paintComponent(Graphics graphic) {
+
+		super.paintComponent(graphic);
+		// drawMenuBoard(g);
+
+		Dimension size = getSize();
+		Insets insets = getInsets();
+
+		menuWidth = size.width - insets.left - insets.right - 1;
+		menuHeight = size.height - insets.top - insets.bottom - 1;
+
+		System.out.println(getInsets().top + ", " + getSize().width + ", " + menuWidth);
+
+		graphic.setColor(new Color(127, 127, 127));
+
+		// Draw the tiles on the board
+		for (int i = 0; i <= ReversiGame.TILES_PER - 1; i++) {
+			for (int j = 0; j <= ReversiGame.TILES_PER - 1; j++) {
+				graphic.setColor(new Color(0, 123, 0));
+				graphic.fillRect(ReversiGame.BOARD_START_X + (ReversiGame.TILE_SIZE * j),
+						ReversiGame.BOARD_START_Y + (ReversiGame.TILE_SIZE * i), ReversiGame.TILE_SIZE,
+						ReversiGame.TILE_SIZE);
+				graphic.setColor(new Color(0, 0, 0));
+				graphic.drawRect(ReversiGame.BOARD_START_X + (ReversiGame.TILE_SIZE * j),
+						ReversiGame.BOARD_START_Y + (ReversiGame.TILE_SIZE * i), ReversiGame.TILE_SIZE,
+						ReversiGame.TILE_SIZE);
+
+				// graphic.drawRect(j * 10, i * 10, 20, 20);
+			}
+		}
+
+		for (int i = 0; i <= ReversiGame.TILES_PER - 1; i++) {
+			for (int j = 0; j <= ReversiGame.TILES_PER - 1; j++) {
+				if (i == j) {
+					graphic.setColor(new Color(255, 255, 255));
+					graphic.fillOval(ReversiGame.PIECE_START_X + (ReversiGame.TILE_SIZE * j),
+							ReversiGame.PIECE_START_Y + (ReversiGame.TILE_SIZE * i), ReversiGame.PIECE_SIZE,
+							ReversiGame.PIECE_SIZE);
+					graphic.setColor(new Color(0, 0, 0));
+					if (i < ReversiGame.TILES_PER / 2) {
+						if (i % 2 == 1) {
+							graphic.fillOval(ReversiGame.PIECE_START_X + (ReversiGame.TILE_SIZE * (j - 1)),
+									ReversiGame.PIECE_START_Y + (ReversiGame.TILE_SIZE * i), ReversiGame.PIECE_SIZE,
+									ReversiGame.PIECE_SIZE);
+							graphic.fillOval(ReversiGame.PIECE_START_X + (ReversiGame.TILE_SIZE * (j + 1)),
+									ReversiGame.PIECE_START_Y + (ReversiGame.TILE_SIZE * i), ReversiGame.PIECE_SIZE,
+									ReversiGame.PIECE_SIZE);
+						}
+					} else {
+						if (i % 2 == 0) {
+							graphic.fillOval(ReversiGame.PIECE_START_X + (ReversiGame.TILE_SIZE * (j - 1)),
+									ReversiGame.PIECE_START_Y + (ReversiGame.TILE_SIZE * i), ReversiGame.PIECE_SIZE,
+									ReversiGame.PIECE_SIZE);
+							graphic.fillOval(ReversiGame.PIECE_START_X + (ReversiGame.TILE_SIZE * (j + 1)),
+									ReversiGame.PIECE_START_Y + (ReversiGame.TILE_SIZE * i), ReversiGame.PIECE_SIZE,
+									ReversiGame.PIECE_SIZE);
+						}
+					}
+				}
+			}
+		}
+		// graphic.drawLine(675, 145, 675, 100);
+	}
+}
--- src/ie/ben/ReversiGame.java
+++ src/ie/ben/ReversiGame.java
@@ -1,92 +1,156 @@
 package ie.ben;
 
-import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 
+import javax.swing.AbstractAction;
 import javax.swing.Box;
-import javax.swing.GroupLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.KeyStroke;
 
 public class ReversiGame extends JFrame {
 
-	//Public constructor for the game
+	MainMenu menu;
+	public static final int TILE_SIZE = 75;
+	public static final int TILES_PER = 8;
+	public static final int BOARD_START_X = 50;
+	public static final int BOARD_START_Y = 150;
+	public static final int PIECE_SIZE = 52;
+	public static final int PIECE_START_X = 63;
+	public static final int PIECE_START_Y = 163;
+	
+	// Public constructor for the game
 	public ReversiGame() {
 
-		//On further reading of different tutorials it seems
-		//that it's common place not to have implementation
-		//code in the constructor for Swing applications
-		//so I've passed off the code to a private method
+		// On further reading of different tutorials it seems
+		// that it's common place not to have implementation
+		// code in the constructor for Swing applications
+		// so I've passed off the code to a private method
 		initGUI();
 	}
-	
-	//Initialise the main window here
+
+	// Initialise the main window here
 	private void initGUI() {
-		
-		//Call the createMenuBar method to set up the initial
-		//window
+
+		// Call the createMenuBar method to set up the initial
+		// window
 		createMenuBar();
-		
-		Container pane = getContentPane();
-        GroupLayout gl = new GroupLayout(pane);
-        pane.setLayout(gl);   
-        
+		menu = new MainMenu();
+		add(menu);
+//		menu = new MainMenu();
+		//add(menu);
+
+		// Create the container windows that holds the listener objects
+//		Container pane = getContentPane();
+//		GroupLayout gl = new GroupLayout(pane);
+//		pane.setLayout(gl);
+
+		// Set the title/size/position/close operation
 		setTitle("Reversi");
 		setSize(700, 900);
 		setLocationRelativeTo(null);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);		
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		
 	}
-	
+
+	// method to create the menu bar of the window
 	private void createMenuBar() {
-		
+
+		// Use the Java Swing libraries to set up appropriate
+		// menu bar features.
 		JMenuBar menuBar = new JMenuBar();
-		
+
 		ImageIcon newIcon = new ImageIcon("new.png");
 		ImageIcon saveIcon = new ImageIcon("save.png");
 		ImageIcon loadIcon = new ImageIcon("load.png");
 		ImageIcon settingsIcon = new ImageIcon("settings.png");
 		ImageIcon exitIcon = new ImageIcon("exit.png");
 		ImageIcon helpIcon = new ImageIcon("help.png");
-		
+
+		// Create a menu called "Game"
 		JMenu gameMenu = new JMenu("Game");
-		
-		JMenuItem newGame = new JMenuItem("New", newIcon);
-		JMenuItem saveGame = new JMenuItem("Save", saveIcon);
-		JMenuItem loadGame = new JMenuItem("Load", loadIcon);
-		JMenuItem exitGame = new JMenuItem("Exit", exitIcon);
+		gameMenu.setMnemonic(KeyEvent.VK_G); //Set a mnemonic for the game menu "Alt-G"
+
+		JMenuItem newGame = new JMenuItem(new MenuItemAction("New", newIcon, 
+				KeyEvent.VK_N));
+		newGame.setToolTipText("Start a new game");
+		newGame.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N,
+				ActionEvent.CTRL_MASK));
+		JMenuItem saveGame = new JMenuItem(new MenuItemAction("Save", saveIcon,
+				KeyEvent.VK_S));
+		saveGame.setToolTipText("Save the current game");
+		JMenuItem loadGame = new JMenuItem(new MenuItemAction("Load", loadIcon,
+				KeyEvent.VK_L));
+		loadGame.setToolTipText("Load the last game");
+		JMenuItem exitGame = new JMenuItem(new MenuItemAction("Exit", exitIcon,
+				KeyEvent.VK_E));
+		exitGame.setToolTipText("Exit the program");
+		exitGame.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_W,
+				ActionEvent.CTRL_MASK));
 		JMenu settings = new JMenu("Settings");
+		settings.setToolTipText("Change the game settings");
 		JMenu helpGame = new JMenu("Help");
-		
-		//Add an ActionListener that exits the application
-//		exitGame.addActionListener(new ActionListener() {
-//			@Override
-//			public void actionPerformed(ActionEvent event) {
-//				System.exit(0);
-//			}
-//		});
-		
+		helpGame.setToolTipText("Help!");
+
+		// Add an ActionListener that exits the application
+		exitGame.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent event) {
+				System.exit(0);
+			}
+		});
+
 		gameMenu.add(newGame);
 		gameMenu.add(saveGame);
 		gameMenu.add(loadGame);
 		gameMenu.add(exitGame);
-		
+
 		menuBar.add(gameMenu);
 		menuBar.add(settings);
 		menuBar.add(Box.createHorizontalGlue());
 		menuBar.add(helpGame);
-		
+
 		setJMenuBar(menuBar);
 	}
 
+	//Create a private inner class to handle the mnemonic and naming of the menu items
+	//This is integral to the windows pane so include it as an inner class
+	private class MenuItemAction extends AbstractAction {
+
+		//Constructor for the MenuItemAction class
+		public MenuItemAction(String text, ImageIcon icon, Integer mnemonic) {
+			super(text);
+
+			putValue(SMALL_ICON, icon);
+			putValue(MNEMONIC_KEY, mnemonic);
+		}
+
+		//Remove this when the menu items have functionality
+		@Override
+		public void actionPerformed(ActionEvent e) {
+
+			System.out.println(e.getActionCommand());
+		}
+	}
+
 	public static void main(String[] args) {
-		
+
+		// The EventQueue invokeLater method is used so the
+		// application doesn't crash.
 		EventQueue.invokeLater(new Runnable() {
-			
+
+			// Runnable() must implement the inherited abstract
+			// run method. This creates the game object and runs
+			// the application loop.
 			@Override
 			public void run() {
 				ReversiGame game = new ReversiGame();
