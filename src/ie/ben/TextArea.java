bdae5dda98b9ec61b52fba9d448a2fad0278e10e
benjimimac <c14723525@mydit.ie>
2016-04-14 12:24:37 +0100

    TextArea is now a class, not a method

--- src/ie/ben/TextArea.java
+++ src/ie/ben/TextArea.java
@@ -40,41 +40,5 @@ public class TextArea extends JPanel{
 		//Add the label to the TextArea JPanel
 		add(label);
 		
-		// In a for loop add two text areas that will show both players progress
-//		for (PlayerObject player : ReversiGame.players) {
-//
-//			// Create a JPanel that will contain the JLabel and set its
-//			// preferences
-//			panel1 = new JPanel();
-//			panel1.setBackground(new Color(41, 41, 41));
-//			panel1.setPreferredSize(new Dimension(ReversiGame.WINDOW_WIDTH / 2, 200));
-//			panel1.setBorder(BorderFactory.createEtchedBorder(20, Color.white, Color.white));
-//
-//			// Add the JLabel
-//			label1 = new JLabel(
-//					"<html>Player: " + playerNum + "<br>PieceCount: " + player.pieceCount + "</html>"); // Player
-//			// details
-//			// will
-//			// go
-//			// here
-//			label1.setFont(new Font("Serif", Font.BOLD, 19));
-//			label1.setLayout(new BorderLayout());
-//			label1.setForeground(Color.white);
-//
-//			// Add the JLabel to the JPanel
-//			panel1.add(label1);
-//
-//			// Position the JPanels appropriately
-//			if (playerNum == 1) {
-//
-//				add(panel1, BorderLayout.LINE_START);
-//			} else {
-//
-//				add(panel1, BorderLayout.LINE_END);
-//			}
-//
-//			// Increment playerNum
-//			playerNum++;
-//		}
 	}
 }
